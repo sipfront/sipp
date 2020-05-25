834697b23b70d27d94077973bcd27215f7dbebe4
Andreas Granig <andreas@granig.com>
2020-05-25 19:27:25 +0000

    Launch idle sipp with helper script
    Use
            mosquitto_pub -h localhost -t /sipp/ctrl -m "cset rate 10"
    to launch traffic

--- agranig-run.sh
+++ agranig-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./sipp -trace_stat -fd 3 -trace_rtt -rtt_freq 100 -mqtt_stats 1 -mqtt_stats_topic '/sipp/stats/call' -mqtt_rttstats_topic '/sipp/stats/rtt' -mqtt_ctrl 1 -mqtt_ctrl_topic '/sipp/ctrl' -sn uac -r 1 -trace_err 127.0.0.1
+./sipp -trace_stat -fd 3 -trace_rtt -rtt_freq 100 -mqtt_stats 1 -mqtt_stats_topic '/sipp/stats/call' -mqtt_rttstats_topic '/sipp/stats/rtt' -mqtt_ctrl 1 -mqtt_ctrl_topic '/sipp/ctrl' -sn uac -r 0 -trace_err 127.0.0.1
