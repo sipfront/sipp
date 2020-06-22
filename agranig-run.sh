da3f5ff54e7df0934d1a41b96f13466c425d40ff
Andreas Granig <andreas@granig.com>
2020-06-22 10:21:19 +0000

    Add UUID session id to MQTT topic

--- agranig-run.sh
+++ agranig-run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source mqtt-auth.conf
+UUID=$(uuidgen)
 
 ./sipp \
     -trace_stat -fd 3 \
     -trace_rtt -rtt_freq 100 \
-    -mqtt_stats 1 -mqtt_stats_topic '/sipp/stats/call' -mqtt_rttstats_topic '/sipp/stats/rtt' \
+    -mqtt_stats 1 -mqtt_stats_topic "/sipp/stats/$UUID/call" -mqtt_rttstats_topic "/sipp/stats/$UUID/rtt" \
     -mqtt_ctrl 1 -mqtt_ctrl_topic '/sipp/ctrl' \
     $MQTT_AUTH \
     -sn uac -r 0 \
