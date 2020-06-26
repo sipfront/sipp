c3fd157eaabe4c2e95b1c0635ee3789d1f0feaa1
Andreas Granig <andreas@granig.com>
2020-06-26 12:56:55 +0200

    Upload docker image to AWS ECR

--- agranig-build.sh
+++ agranig-build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-docker build -f docker/Dockerfile .
+set -e
 
+aws ecr get-login-password --region eu-central-1 --profile Administrator | \
+    docker login \
+    --username AWS \
+    --password-stdin \
+    138885365874.dkr.ecr.eu-central-1.amazonaws.com
 
+docker build -f docker/Dockerfile.sipfront -t sipfront-sipp .
+
+docker tag sipfront-sipp:latest \
+    138885365874.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
+
+docker push \
+    138885365874.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
