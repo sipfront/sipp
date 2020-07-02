e7ce982cc0550b316a8e890c4f784884a9dacac0
Andreas Granig <andreas@granig.com>
2020-07-02 11:12:40 +0000

    Customize cseq and cid

--- agranig-run-manual.sh
+++ agranig-run-manual.sh
@@ -4,8 +4,14 @@ source mqtt-auth.conf
 #UUID=$(uuidgen)
 UUID='ecabb78c-5900-4c38-a63b-4e54ecadf642'
 
+# -nd -default_behaviors: no defaults, but abort on unexpected message
+# -aa: auto-answer 200 for INFO, NOTIFY, OPTIONS, UPDATE \
+
 ./sipp \
     -nd -default_behaviors abortunexp \
+    -aa \
+    -cid_str 'sipfront-%u-%p@%s' \
+    -base_cseq 1 \
     -trace_stat -fd 1 \
     -trace_rtt -rtt_freq 1 \
     -mqtt_stats 1 -mqtt_stats_topic "/sipp/stats/$UUID/call" -mqtt_rttstats_topic "/sipp/stats/$UUID/rtt" \
