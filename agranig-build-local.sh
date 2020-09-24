3077ae3475e492469d30d1ba360e9d0249b29d54
Andreas Granig <andreas@granig.com>
2020-09-24 21:08:36 +0200

    Remove deprecated scenario copying

--- agranig-build-local.sh
+++ agranig-build-local.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-pushd ../sipfront-scenarios
-git u
-popd
-
-test -d sipfront-scenarios && rm -rf sipfront-scenarios
-cp -r ../sipfront-scenarios .
-
 docker build -f docker/Dockerfile.sipfront -t sipfront-sipp:latest .
 
 rm -rf sipfront-scenarios
