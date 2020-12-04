6843ba9524e8c91162557cadca0d074bff3867f1
Andreas Granig <andreas@granig.com>
2020-12-04 12:31:02 +0100

    Make AWS settings configurable

--- agranig-build.sh
+++ agranig-build.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+PROFILE="granig-admin"
+AWS_ID="138885365874"
+
 set -e
 
-aws ecr get-login-password --region eu-central-1 --profile Administrator | \
+aws ecr get-login-password --region eu-central-1 --profile ${PROFILE} | \
     docker login \
     --username AWS \
     --password-stdin \
-    138885365874.dkr.ecr.eu-central-1.amazonaws.com
+    ${AWS_ID}.dkr.ecr.eu-central-1.amazonaws.com
 
 docker build -f docker/Dockerfile.sipfront -t sipfront-sipp .
 
 docker tag sipfront-sipp:latest \
-    138885365874.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
+    ${AWS_ID}.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
 
 docker push \
-    138885365874.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
+    ${AWS_ID}.dkr.ecr.eu-central-1.amazonaws.com/sipfront-sipp:latest
