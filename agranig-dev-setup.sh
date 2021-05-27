45b858887a2bb8de795f13e078a55fc4d7776ec6
Andreas Granig <andreas@granig.com>
2021-05-27 11:11:07 +0200

    Add dev helpers

--- agranig-dev-setup.sh
+++ agranig-dev-setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y libsctp1 uuid-runtime build-essential cmake git libsctp-dev libncurses-dev libpcap-dev libcurl4-openssl-dev libgsl-dev libmosquitto-dev libssl-dev ack vim gdb
+docker run -it -v $(pwd):/sipp debian:stable /bin/bash /sipp/agranig-dev-helper.sh
